793063d365f031635c5f5b9c26321dc2d1f1533d
Leiko <leticia.lkonno@gmail.com>
2019-10-08 14:37:07 -0300

    add eureka server and configuration

--- ContentService/src/main/java/edu/fiap/netflix/NetflixApplication.java
+++ ContentService/src/main/java/edu/fiap/netflix/NetflixApplication.java
@@ -2,10 +2,12 @@ package edu.fiap.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@RefreshScope
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class NetflixApplication{
 	
 	public static void main(String[] args) {
--- ProfileService/src/main/java/edu/fiap/netflix/NetflixApplication.java
+++ ProfileService/src/main/java/edu/fiap/netflix/NetflixApplication.java
@@ -1,15 +1,15 @@
 package edu.fiap.netflix;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class NetflixApplication{
-
-	private static final Logger log = LoggerFactory.getLogger(NetflixApplication.class);
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixApplication.class, args);
 	}
