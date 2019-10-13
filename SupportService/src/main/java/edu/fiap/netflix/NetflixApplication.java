8644e51435e229e0f17186be1b5306bb0bd90c79
leiko <leticia.lkonno@gmail.com>
2019-10-13 11:50:42 -0300

    remove eureka target dir and db dependency to support service

--- SupportService/src/main/java/edu/fiap/netflix/NetflixApplication.java
+++ SupportService/src/main/java/edu/fiap/netflix/NetflixApplication.java
@@ -2,12 +2,8 @@ package edu.fiap.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableCircuitBreaker
 public class NetflixApplication{
 	
 	public static void main(String[] args) {
--- SupportService/src/main/java/edu/fiap/netflix/service/Consumer.java
+++ SupportService/src/main/java/edu/fiap/netflix/service/Consumer.java
@@ -1,7 +1,6 @@
 package edu.fiap.netflix.service;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.slf4j.Logger;
